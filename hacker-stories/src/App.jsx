abe2471c74bb4b5f62f6ff4ea72cb4800b2988b7
Chen Lin <fdu_linchen@hotmail.com>
2026-05-03 21:23:46 -0500

    add eslint

--- hacker-stories/src/App.jsx
+++ hacker-stories/src/App.jsx
@@ -1,17 +1,46 @@
-import * as React from 'react'
+import 'react'
 
 const welcome = {
   greeting: 'Hey, ',
   title: 'there',
 }
 
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  }]
+
 function App() {
-  const title = "React"
   return (
     <div>
       <h1 style={{ fontFamily: 'Roboto, sans-serif' }}>{welcome.greeting} {welcome.title}</h1>
       <label htmlFor='search'>Search: </label>
       <input id='search' type='text'></input>
+      <hr />
+      <ul>
+        {list.map(function (item) {
+          return <li key={item.objectID} style={{ display: 'flex', gap: '1rem' }}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span></li>;
+        })}
+      </ul>
     </div>
 
   )
